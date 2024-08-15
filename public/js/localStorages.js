7dd216dd13f33e0ff838765c70dbf1a27d169966
Pachakutiq <null@gmail.com>
2024-08-15 23:09:20 +0800

    fix: Sync with EasyCanvas-builds and EasyCanvas-desktop

--- public/js/localStorages.js
+++ public/js/localStorages.js
@@ -1,28 +1,29 @@
 // Getting server URL from local storage
 function getServerURL() {
     // If no server URL in local storage, prompt the user to enter it
-    var serverURL = localStorage.getItem("serverURL");
+    // var serverURL = localStorage.getItem("serverURL");
 
-    var defaultURL = "http://localhost:9283";
+    var defaultURL = "http://127.0.0.1:9283";
 
-    if (!serverURL) {
-        serverURL = window.prompt("Please enter the server URL:", defaultURL);
-        // If closed without entering anything, use the default URL
-        if (!serverURL) {
-            serverURL = defaultURL;
-        }
-        // If url not start with http:// or https://, add it
-        if (!serverURL.startsWith("http://") && !serverURL.startsWith("https://")) {
-            serverURL = "http://" + serverURL;
-        }
-        // If url end with /, remove it
-        if (serverURL.endsWith("/")) {
-            serverURL = serverURL.slice(0, -1);
-        }
-        localStorage.setItem("serverURL", serverURL);
-    }
+    // if (!serverURL) {
+    //     serverURL = window.prompt("Please enter the server URL:", defaultURL);
+    //     // If closed without entering anything, use the default URL
+    //     if (!serverURL) {
+    //         serverURL = defaultURL;
+    //     }
+    //     // If url not start with http:// or https://, add it
+    //     if (!serverURL.startsWith("http://") && !serverURL.startsWith("https://")) {
+    //         serverURL = "http://" + serverURL;
+    //     }
+    //     // If url end with /, remove it
+    //     if (serverURL.endsWith("/")) {
+    //         serverURL = serverURL.slice(0, -1);
+    //     }
+    //     localStorage.setItem("serverURL", serverURL);
+    // }
 
-    return serverURL;
+    // return serverURL;
+    return defaultURL
 }
 
 function getAccessTokenHeaders() {
